d9787f9aecfcc9d4690479648765105ea7750a3c
corantinspee <37345787+corantinspee@users.noreply.github.com>
2019-05-22 16:41:51 +0200

    Add files via upload

---
+++ Site Corantin/Public/index.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+        <div id="ecusson" class="centre">Corantin SPEE</div>
+        <nav class="navigation">
+            <a class="navBtn centre" href="#home">Home</a>
+            <a class="navBtn centre" href="#formation">Formations</a>
+            <a class="navBtn centre" href="#experiences">Experiences</a>
+            <a class="navBtn centre" href="#portfolio">Port-folio</a>
+            <a class="navBtn centre" href="#contact">Me contacter</a>
+        </nav>
+    </header>
+    <section id="home">
+        <div class="sectionTitle">Qui suis-je ?</div>
+        <div class="ligne maxwidth spaceevenly">
+            <div class="caractere">
+                <img src="img/support.svg" alt="">
+                <h2>Teamwork</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero, fugiat commodi nesciunt optio suscipit quisquam! Doloribus quae aperiam hic aliquam nemo, aut accusamus sequi expedita numquam ipsa dolorem facere provident!</p>
+            </div>
+            <div class="caractere">
+                <img src="img/lightbulb.svg" alt="">
+                <h2>Creativity</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero, fugiat commodi nesciunt optio suscipit quisquam! Doloribus quae aperiam hic aliquam nemo, aut accusamus sequi expedita numquam ipsa dolorem facere provident!</p>
+            </div>
+            <div class="caractere">
+                <img src="img/" alt="">
+                <h2>Persistent</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero, fugiat commodi nesciunt optio suscipit quisquam! Doloribus quae aperiam hic aliquam nemo, aut accusamus sequi expedita numquam ipsa dolorem facere provident!</p>
+            </div>
+        </div>
+    </section>
+    <section id="formation">
+        <div class="sectionTitle">Formations</div>
+        <h1>Try to add or remove slides</h1>
+<div class="container">
+        <div class="prev">
+        	<div class="arrow-left"></div>
+        </div>
+        <ul class="slider">
+            <li class="visible">
+                <img src="img/téléchargement.jpg" alt="" /> 
+            </li>
+            <li>
+                <img src="img/lamache.jpg" alt="" />
+            </li>
+            <li>
+                <img src="http://photoserge.com/img/course-images/trey-ratcliff-lake-from-above.jpg" alt="" /> 
+          </li>
+          <li><img src="http://www.hdwallpapers.in/walls/lake_hawea_new_zealand-wide.jpg" alt="" /></li>
+        </ul>
+        <div class="next">
+        	<div class="arrow-right"></div>
+        </div>
+    </div>
+            
+    </section>
+    <section id="experiences">
+        <div class="sectionTitle">Experiences</div>
+    </section>
+    <section id="portfolio">
+        <div class="sectionTitle">Portfolio</div>
+    </section>
+    <section id="contact">
+            <div class="ligne maxwidth spaceevenly">
+        <div class="sectionTitle">Contact</div>
+        <div class="social">
+            <img src="img/opened-email-envelope.svg">Envoyez un mail</a>
+        </div>
+        <div class="social">
+            <img src="img/smartphone-call.svg">0619485781</a></li>
+        </div>
+        <div class="social">
+            
+        </div>
+        <a class="logo" href="https://www.linkedin.com/in/corantin-spee-69950b11b/"><img src="img/in.png" ></a>
+        <a class="logo" href=""><img src="img/facebook.svg" class="logo"></a>
+            
+        </section>
+
+<script src="app.js"></script>
+</body>
+</html>
